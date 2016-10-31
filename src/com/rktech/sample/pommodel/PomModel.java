ea6dba35c09ce85469e0b40f86dc865da120f0aa
I333245 <rajkumar.selvam@sap.com>
2016-10-31 15:05:39 +0530

    Few modifications

--- src/com/rktech/sample/pommodel/PomModel.java
+++ src/com/rktech/sample/pommodel/PomModel.java
@@ -14,30 +14,18 @@ public class PomModel {
 	 */
 	public static void main(String[] args) {
 		PomReader pReader = new PomReader();
-		/*
-		<dependency>
-			<groupId>com.sap.bdh</groupId>
-			<artifactId>bdh-tools-project-type</artifactId>
-			<version>1.0.0-SNAPSHOT</version>
-		</dependency>
-		<dependency>
-			<groupId>com.sap.bdh</groupId>
-			<artifactId>bdh-tools-project-api</artifactId>
-			<version>1.0.0-SNAPSHOT</version>
-		</dependency>
-		*/
 		Model model = pReader.readPom();
-        Dependency bdhProjectTypeDep = new Dependency();
-        Dependency bdhProjectApiDep = new Dependency();
-        bdhProjectTypeDep.setGroupId("com.sap.bdh");
-        bdhProjectTypeDep.setArtifactId("bdh-tools-project-type");
-        bdhProjectTypeDep.setVersion("1.0.0-SNAPSHOT");
-        bdhProjectApiDep.setGroupId("com.sap.bdh");
-        bdhProjectApiDep.setArtifactId("bdh-tools-project-api");
-        bdhProjectApiDep.setVersion("1.0.0-SNAPSHOT");
+        Dependency dep1 = new Dependency();
+        Dependency dep2 = new Dependency();
+        dep1.setGroupId("new-groupid");
+        dep1.setArtifactId("new-artifact-1");
+        dep1.setVersion("1.0.0-SNAPSHOT");
+        dep2.setGroupId("new-groupid");
+        dep2.setArtifactId("new-artifact-1");
+        dep2.setVersion("1.0.0-SNAPSHOT");
         
-        model.addDependency(bdhProjectTypeDep);
-        model.addDependency(bdhProjectApiDep);
+        model.addDependency(dep1);
+        model.addDependency(dep2);
         
         PomWriter pWriter = new PomWriter();
         boolean status = pWriter.writePom(model);
